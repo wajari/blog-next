5adf71ea1fdaa62a5dcdbb9dc7cd89ad27068f81
wajari <wajari@gmail.com>
2020-06-18 19:40:31 +0200

    Searching for bugs in 404 errors in deployment

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8274b417365104fc3f42";
+/******/ 	var hotCurrentHash = "0e71c25556e7936312c3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- my-worker.js
+++ my-worker.js
@@ -15,14 +15,14 @@ the directory.
 
 const precacheFiles = [
   "/",
-  "/about/",
-  "/search/",
-  "/blog/",
-  "/blog/post-one-code-block-demo/",
-  "/blog/post-two-image-demo/",
-  "/blog/post-three-smooth-scroll-and-reading-progress-bar/",
-  "/blog/post-four-link-on-twitter/",
-  "/blog/blog-react/",
+  "/about",
+  "/search",
+  "/blog",
+  "/blog/post-one-code-block-demo",
+  "/blog/post-two-image-demo",
+  "/blog/post-three-smooth-scroll-and-reading-progress-bar",
+  "/blog/post-four-link-on-twitter",
+  "/blog/blog-react",
 ];
 
 self.addEventListener("install", (e) => {
