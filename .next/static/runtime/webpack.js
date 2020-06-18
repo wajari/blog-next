017eabe7d01bb6b09e4f7bce86ce275f746729b2
wajari <wajari@gmail.com>
2020-06-18 19:18:25 +0200

    Minor changes in search for bugs in route and 404 in production

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "85742678cefb83a822b8";
+/******/ 	var hotCurrentHash = "8274b417365104fc3f42";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
